3e503648bdb487ae065c4996700e6757b83496d0
neustupov <neustupov@yandex.ru>
2018-02-16 07:48:26 +0300

    add impl for CrudRestaurantRepository and DataJpaRestaurantRepositoryImpl

--- src/main/java/ru/neustupov/votingForRestaurants/repository/RestaurantRepository.java
+++ src/main/java/ru/neustupov/votingForRestaurants/repository/RestaurantRepository.java
@@ -1,4 +1,26 @@
 package ru.neustupov.votingForRestaurants.repository;
 
+import ru.neustupov.votingForRestaurants.model.Restaurant;
+
+import java.util.List;
+
 public interface RestaurantRepository {
+
+    Restaurant save(Restaurant user);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Restaurant get(int id);
+
+    List<Restaurant> getAll();
+
+    default Restaurant getWithVotes(int id){
+        throw new UnsupportedOperationException();
+    }
+
+    default Restaurant getWithMenus(int id){
+        throw new UnsupportedOperationException();
+    }
 }
