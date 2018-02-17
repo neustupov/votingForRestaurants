f2e096aca0b1e74a4d4927cb93e2922ebe765ab2
neustupov <neustupov@yandex.ru>
2018-02-17 13:17:19 +0300

    implemented Vote Services interface and class

--- src/main/java/ru/neustupov/votingForRestaurants/service/VoteService.java
+++ src/main/java/ru/neustupov/votingForRestaurants/service/VoteService.java
@@ -1,4 +1,25 @@
 package ru.neustupov.votingForRestaurants.service;
 
+import ru.neustupov.votingForRestaurants.model.Vote;
+import ru.neustupov.votingForRestaurants.util.exception.NotFoundException;
+
+import java.util.List;
+
 public interface VoteService {
+
+    Vote create(Vote vote);
+
+    void delete(int id) throws NotFoundException;
+
+    Vote get(int id) throws NotFoundException;
+
+    void update(Vote vote);
+
+    List<Vote> getAll();
+
+    Vote getWithUser(int id);
+
+    Vote getWithRestaurant(int id);
+
+    Vote getWithUserAndRestaurant(int id);
 }
--- src/main/java/ru/neustupov/votingForRestaurants/service/VoteServiceImpl.java
+++ src/main/java/ru/neustupov/votingForRestaurants/service/VoteServiceImpl.java
@@ -1,4 +1,61 @@
 package ru.neustupov.votingForRestaurants.service;
 
-public class VoteServiceImpl {
+import org.springframework.util.Assert;
+import ru.neustupov.votingForRestaurants.model.Vote;
+import ru.neustupov.votingForRestaurants.repository.VoteRepository;
+import ru.neustupov.votingForRestaurants.util.exception.NotFoundException;
+
+import java.util.List;
+
+import static ru.neustupov.votingForRestaurants.util.ValidationUtil.checkNotFoundWithId;
+
+public class VoteServiceImpl implements VoteService{
+
+    private final VoteRepository repository;
+
+    public VoteServiceImpl(VoteRepository voteRepository){
+        this.repository = voteRepository;
+    }
+
+    @Override
+    public Vote create(Vote vote) {
+        Assert.notNull(vote, "vote must not be null");
+        return repository.save(vote);
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    @Override
+    public Vote get(int id) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    @Override
+    public void update(Vote vote) {
+        Assert.notNull(vote, "vote must not be null");
+        checkNotFoundWithId(repository.save(vote), vote.getId());
+    }
+
+    @Override
+    public List<Vote> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public Vote getWithUser(int id) {
+        return checkNotFoundWithId(repository.getWithUser(id), id);
+    }
+
+    @Override
+    public Vote getWithRestaurant(int id) {
+        return checkNotFoundWithId(repository.getWithRestaurant(id), id);
+    }
+
+    @Override
+    public Vote getWithUserAndRestaurant(int id) {
+        return checkNotFoundWithId(repository.getWithRestaurantAndUser(id), id);
+    }
 }
