afe31f44637f4dcccd1408710d1021210d90ad1b
neustupov <neustupov@yandex.ru>
2018-02-17 12:44:49 +0300

    add ValidationUtil class, implemented User Services classes

--- src/main/java/ru/neustupov/votingForRestaurants/service/UserServiceImpl.java
+++ src/main/java/ru/neustupov/votingForRestaurants/service/UserServiceImpl.java
@@ -2,14 +2,17 @@ package ru.neustupov.votingForRestaurants.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import ru.neustupov.votingForRestaurants.model.User;
 import ru.neustupov.votingForRestaurants.repository.UserRepository;
 import ru.neustupov.votingForRestaurants.util.exception.NotFoundException;
 
 import java.util.List;
 
+import static ru.neustupov.votingForRestaurants.util.ValidationUtil.checkNotFoundWithId;
+
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
 
@@ -20,26 +23,33 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User create(User user) {
-        return null;
+        Assert.notNull(user, "user must not be null");
+        return repository.save(user);
     }
 
     @Override
     public void delete(int id) throws NotFoundException {
-
+        checkNotFoundWithId(repository.delete(id), id);
     }
 
     @Override
     public User get(int id) throws NotFoundException {
-        return null;
+        return checkNotFoundWithId(repository.get(id), id);
     }
 
     @Override
     public void update(User user) {
-
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.getId());
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return repository.getAll();
+    }
+
+    @Override
+    public User getWithVotes(int id) {
+        return checkNotFoundWithId(repository.getWithVotes(id), id);
     }
 }
