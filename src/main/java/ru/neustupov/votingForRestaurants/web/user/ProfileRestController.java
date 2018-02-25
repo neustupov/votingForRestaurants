397e4abd6f7bd4854362fd183c19f9fe336d29e8
neustupov <neustupov@yandex.ru>
2018-02-25 11:21:08 +0300

    renamed and moved to user directory

--- src/main/java/ru/neustupov/votingForRestaurants/web/user/ProfileRestController.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/user/ProfileRestController.java
@@ -1,9 +1,9 @@
-package ru.neustupov.votingForRestaurants.web;
+package ru.neustupov.votingForRestaurants.web.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UserRestController {
+public class ProfileRestController {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 }
