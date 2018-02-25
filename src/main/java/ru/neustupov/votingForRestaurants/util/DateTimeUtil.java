24168ce71d17be0e2c8acd2cf70bde61b7426bb6
neustupov <neustupov@yandex.ru>
2018-02-25 10:58:51 +0300

    added for future formatted

---
+++ src/main/java/ru/neustupov/votingForRestaurants/util/DateTimeUtil.java
@@ -0,0 +1,13 @@
+package ru.neustupov.votingForRestaurants.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtil {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static String toString(LocalDateTime ldt) {
+        return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+}
