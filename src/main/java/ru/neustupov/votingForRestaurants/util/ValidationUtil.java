6f7fb2627fcf9a2c48327a4612f6c113f1d9fa4b
neustupov <neustupov@yandex.ru>
2018-02-25 13:27:08 +0300

    added new methods for check entities

--- src/main/java/ru/neustupov/votingForRestaurants/util/ValidationUtil.java
+++ src/main/java/ru/neustupov/votingForRestaurants/util/ValidationUtil.java
@@ -1,5 +1,6 @@
 package ru.neustupov.votingForRestaurants.util;
 
+import ru.neustupov.votingForRestaurants.model.AbstractBaseEntity;
 import ru.neustupov.votingForRestaurants.util.exception.NotFoundException;
 
 public class ValidationUtil {
@@ -20,6 +21,21 @@ public class ValidationUtil {
     public static void checkNotFound(boolean found, String msg) {
         if (!found) {
             throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractBaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
+//      http://stackoverflow.com/a/32728226/548473
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
         }
     }
 }
