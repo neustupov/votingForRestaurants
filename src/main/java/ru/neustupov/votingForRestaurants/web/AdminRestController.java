4e347326a6a4223cabb6d5e0f1db18ef880f4351
neustupov <neustupov@yandex.ru>
2018-02-25 10:57:38 +0300

    add logger

--- src/main/java/ru/neustupov/votingForRestaurants/web/AdminRestController.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/AdminRestController.java
@@ -1,4 +1,9 @@
 package ru.neustupov.votingForRestaurants.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AdminRestController {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 }
