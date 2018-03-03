b9569126ecb0ca3b186376b038dd6adf088fde53
neustupov <neustupov@yandex.ru>
2018-03-03 11:06:22 +0300

    added all methods

--- src/main/java/ru/neustupov/votingForRestaurants/web/user/ProfileRestController.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/user/ProfileRestController.java
@@ -2,8 +2,33 @@ package ru.neustupov.votingForRestaurants.web.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.neustupov.votingForRestaurants.AuthorizedUser;
+import ru.neustupov.votingForRestaurants.model.User;
+import ru.neustupov.votingForRestaurants.service.UserService;
 
+@Controller
 public class ProfileRestController {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private UserService service;
+
+    @Autowired
+    public ProfileRestController(UserService service) {
+        this.service = service;
+    }
+
+    public User get() {
+        return service.get(AuthorizedUser.id());
+    }
+
+    public void delete() {
+        service.delete(AuthorizedUser.id());
+    }
+
+    public void update(User user) {
+        service.update(user);
+    }
 }
