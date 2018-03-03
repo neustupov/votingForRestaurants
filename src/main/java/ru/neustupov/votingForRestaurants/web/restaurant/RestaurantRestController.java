30ab87fb51847633453ea951dc6499bff4bb7671
neustupov <neustupov@yandex.ru>
2018-03-03 11:06:58 +0300

    added all methods

--- src/main/java/ru/neustupov/votingForRestaurants/web/restaurant/RestaurantRestController.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/restaurant/RestaurantRestController.java
@@ -1,8 +1,55 @@
 package ru.neustupov.votingForRestaurants.web.restaurant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.neustupov.votingForRestaurants.AuthorizedUser;
+import ru.neustupov.votingForRestaurants.model.Restaurant;
 import ru.neustupov.votingForRestaurants.service.RestaurantService;
 
+import java.util.List;
+
+import static ru.neustupov.votingForRestaurants.util.ValidationUtil.assureIdConsistent;
+import static ru.neustupov.votingForRestaurants.util.ValidationUtil.checkNew;
+
+@Controller
 public class RestaurantRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(RestaurantRestController.class);
+
     private RestaurantService service;
+
+    @Autowired
+    public RestaurantRestController(RestaurantService service){
+        this.service = service;
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        checkNew(restaurant);
+        log.info("create {}", restaurant);
+        return service.create(restaurant);
+    }
+
+    public void update(Restaurant restaurant, int id) {
+        int userId = AuthorizedUser.id();
+        log.info("update {} with id = {}", restaurant, id);
+        assureIdConsistent(restaurant, id);
+        service.update(restaurant);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public Restaurant get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public List<Restaurant> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
 }
