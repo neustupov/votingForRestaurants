276ca7286050171114784c10bd783f43d7849ca3
neustupov <neustupov@yandex.ru>
2018-03-03 18:03:31 +0300

    added variables and edited init of votes

--- src/main/java/ru/neustupov/votingForRestaurants/util/MockUtil.java
+++ src/main/java/ru/neustupov/votingForRestaurants/util/MockUtil.java
@@ -7,32 +7,46 @@ import ru.neustupov.votingForRestaurants.model.Vote;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MockUtil {
 
-    public static final List<Meal> MEALS = Arrays.asList(
-            new Meal(0, "soup", 0, 100),
-            new Meal(1, "pasta", 0, 200),
-            new Meal(2, "juice", 0, 50)
+    private static AtomicInteger counterMeals = new AtomicInteger(0);
+    private static AtomicInteger counterVotes = new AtomicInteger(0);
+    private static AtomicInteger counterRestaurants = new AtomicInteger(0);
+    private static AtomicInteger counterMenus = new AtomicInteger(0);
+
+    public static List<Meal> MEALS = Arrays.asList(
+            new Meal(counterMeals.incrementAndGet(), "soup", 1, 100),
+            new Meal(counterMeals.incrementAndGet(), "pasta", 2, 200),
+            new Meal(counterMeals.incrementAndGet(), "juice", 3, 50)
     );
 
-    public static final List<Vote> VOTES = Arrays.asList(
-            new Vote(0, LocalDateTime.of(2015, Month.MAY, 30, 10, 0)),
-            new Vote(1, LocalDateTime.of(2015, Month.JULY, 10, 11, 0))
+
+    public static List<Vote> VOTES = new ArrayList<>();
+
+    static {
+        VOTES.add(new Vote(counterVotes.incrementAndGet(), LocalDateTime.of(2015, Month.MAY, 30, 10, 0)));
+        VOTES.add(new Vote(counterVotes.incrementAndGet(), LocalDateTime.of(2015, Month.JULY, 10, 11, 0)));
+    }
+
+    public static List<Restaurant> RESTAURANTS = Arrays.asList(
+            new Restaurant(counterRestaurants.incrementAndGet(), "Topaz"),
+            new Restaurant(counterRestaurants.incrementAndGet(), "Gruzia"),
+            new Restaurant(counterRestaurants.incrementAndGet(), "Afonja")
     );
 
-    public static final List<Restaurant> RESTAURANTS = Arrays.asList(
-            new Restaurant(0, "Topaz"),
-            new Restaurant(1,"Gruzia"),
-            new Restaurant(2, "Afonja")
+    public static List<Menu> MENUS = Arrays.asList(
+            new Menu(counterMenus.incrementAndGet(), 1, LocalDateTime.of(2015, Month.MAY, 29, 10, 0)),
+            new Menu(counterMenus.incrementAndGet(), 1, LocalDateTime.of(2015, Month.MAY, 30, 10, 0)),
+            new Menu(counterMenus.incrementAndGet(), 2, LocalDateTime.of(2015, Month.MAY, 29, 10, 0)),
+            new Menu(counterMenus.incrementAndGet(), 3, LocalDateTime.of(2015, Month.MAY, 29, 10, 0))
     );
 
-    public static final List<Menu> MENUS = Arrays.asList(
-            new Menu(0, 0, LocalDateTime.of(2015, Month.MAY, 29, 10, 0)),
-            new Menu(1, 0, LocalDateTime.of(2015, Month.MAY, 30, 10, 0)),
-            new Menu(2, 1, LocalDateTime.of(2015, Month.MAY, 29, 10, 0)),
-            new Menu(3, 2, LocalDateTime.of(2015, Month.MAY, 29, 10, 0))
-    );
+    public static Integer getCounterForVote() {
+        return counterVotes.incrementAndGet();
+    }
 }
