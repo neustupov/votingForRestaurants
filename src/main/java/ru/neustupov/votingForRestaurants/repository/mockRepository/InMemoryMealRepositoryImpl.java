c5d3698c36f9754d07b6bd68eefe7724e8a3a8f4
neustupov <neustupov@yandex.ru>
2018-03-04 14:17:50 +0300

    added methods and init

--- src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryMealRepositoryImpl.java
@@ -1,50 +1,69 @@
 package ru.neustupov.votingForRestaurants.repository.mockRepository;
 
+import org.springframework.stereotype.Repository;
 import ru.neustupov.votingForRestaurants.model.Meal;
 import ru.neustupov.votingForRestaurants.repository.MealRepository;
-import ru.neustupov.votingForRestaurants.util.MockUtil;
 
-import java.time.LocalDateTime;
-import java.time.Month;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static ru.neustupov.votingForRestaurants.repository.mockRepository.InMemoryUserRepository.USER_ID;
-
+@Repository
 public class InMemoryMealRepositoryImpl implements MealRepository{
 
     private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
+    {
+        save(new Meal(3, "vine", 300), 3);
+        save(new Meal(3, "tea", 10), 3);
+        save(new Meal(1, "soup", 100), 1);
+        save(new Meal(2, "pasta", 200), 2);
+        save(new Meal(2, "juice", 50), 2);
+    }
+
     @Override
-    public Meal save(Meal user) {
-        return null;
+    public Meal save(Meal meal, int menuId) {
+        Map<Integer, Meal> meals = repository.computeIfAbsent(menuId, ConcurrentHashMap::new);
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+            meals.put(meal.getId(), meal);
+            return meal;
+        }
+        return meals.computeIfPresent(meal.getId(), (id, oldmeal) -> meal);
     }
 
     @Override
     public boolean delete(int id, int menuId) {
-        return false;
+        Map<Integer, Meal> meals = repository.get(menuId);
+        return meals != null && meals.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return null;
+    public Meal get(int id, int menuId) {
+        Map<Integer, Meal> meals = repository.get(menuId);
+        return meals == null ? null : meals.get(id);
     }
 
     @Override
-    public List<Meal> getAll() {
-        return null;
-    }
-
-    @Override
-    public List<Meal> getAllByIdMenu(int idRest) {
-        return null;
+    public List<Meal> getAll(int menuId) {
+        return getAllAsStream(menuId).collect(Collectors.toList());
     }
 
     @Override
     public Meal getWithMenu(int id) {
         return null;
+    }
+
+    private Stream<Meal> getAllAsStream(int menuId) {
+        Map<Integer, Meal> meals = repository.get(menuId);
+        return meals == null ?
+                Stream.empty() :
+                meals.values().stream()
+                        .sorted(Comparator.comparing(Meal::getId));
     }
 }
