f081ae0d1505e9622d6350eb15d46c4a5c7afaec
neustupov <neustupov@yandex.ru>
2018-03-04 14:23:46 +0300

    added init and edited "doGet" method

--- src/main/java/ru/neustupov/votingForRestaurants/web/meal/MealServlet.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/meal/MealServlet.java
@@ -1,15 +1,16 @@
 package ru.neustupov.votingForRestaurants.web.meal;
 
 import org.slf4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.neustupov.votingForRestaurants.model.Meal;
-import ru.neustupov.votingForRestaurants.util.MockUtil;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,24 +20,36 @@ public class MealServlet extends HttpServlet {
 
     private static final Logger log = getLogger(MealServlet.class);
 
+    private ConfigurableApplicationContext springContext;
+    private MealRestController restController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        restController = springContext.getBean(MealRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-
-        List<Meal> meals = MockUtil.MEALS;
 
         String action = request.getParameter("action");
 
         switch (action == null ? "all" : action) {
             case "all":
             default:
-                Integer menuId = Integer.parseInt(request.getParameter("id"));
                 Integer restId = Integer.parseInt(request.getParameter("restId"));
-                List<Meal> filteredMeals = meals.stream()
-                        .filter(meal -> (meal.getIdMenu() == menuId))
-                        .collect(Collectors.toList());
+                Integer menuId = Integer.parseInt(request.getParameter("id"));
+                List<Meal> meals = restController.getAll(menuId);
                 request.setAttribute("restId", restId);
-                request.setAttribute("mealsList", filteredMeals);
+                request.setAttribute("mealsList", meals);
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
