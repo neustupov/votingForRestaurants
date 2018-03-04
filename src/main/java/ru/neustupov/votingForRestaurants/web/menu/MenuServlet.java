a289095731d3ed9dad05e2bdb4e564097a9aa411
neustupov <neustupov@yandex.ru>
2018-03-04 14:25:14 +0300

    added init for context and edited "doGet" method

--- src/main/java/ru/neustupov/votingForRestaurants/web/menu/MenuServlet.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/menu/MenuServlet.java
@@ -1,16 +1,17 @@
 package ru.neustupov.votingForRestaurants.web.menu;
 
 import org.slf4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.neustupov.votingForRestaurants.model.Menu;
-import ru.neustupov.votingForRestaurants.util.MockUtil;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,11 +19,25 @@ public class MenuServlet extends HttpServlet{
 
     private static final Logger log = getLogger(MenuServlet.class);
 
+    private ConfigurableApplicationContext springContext;
+    private MenuRestController restController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        restController = springContext.getBean(MenuRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to menu");
-
-        List<Menu> menus = MockUtil.MENUS;
 
         String action = request.getParameter("action");
 
@@ -30,11 +45,9 @@ public class MenuServlet extends HttpServlet{
             case "all":
             default:
                 Integer restId = Integer.parseInt(request.getParameter("id"));
-                List<Menu> filteredMenus = menus.stream()
-                        .filter(menu -> (menu.getRestId() == restId))
-                        .collect(Collectors.toList());
+                List<Menu> menus = restController.getAll(restId);
                 request.setAttribute("restId", restId);
-                request.setAttribute("menusList", filteredMenus);
+                request.setAttribute("menusList", menus);
                 request.getRequestDispatcher("/menus.jsp").forward(request, response);
                 break;
         }
