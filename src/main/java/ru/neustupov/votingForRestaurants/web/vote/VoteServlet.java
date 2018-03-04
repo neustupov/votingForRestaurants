4f47733a83ef367802763610d083078f273952af
neustupov <neustupov@yandex.ru>
2018-03-04 14:31:43 +0300

    added init context and edited "doGet" method

--- src/main/java/ru/neustupov/votingForRestaurants/web/vote/VoteServlet.java
+++ src/main/java/ru/neustupov/votingForRestaurants/web/vote/VoteServlet.java
@@ -1,9 +1,16 @@
 package ru.neustupov.votingForRestaurants.web.vote;
 
+import org.slf4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.neustupov.votingForRestaurants.AuthorizedUser;
 import ru.neustupov.votingForRestaurants.model.Restaurant;
+import ru.neustupov.votingForRestaurants.model.User;
 import ru.neustupov.votingForRestaurants.model.Vote;
-import ru.neustupov.votingForRestaurants.util.MockUtil;
+import ru.neustupov.votingForRestaurants.web.restaurant.RestaurantRestController;
+import ru.neustupov.votingForRestaurants.web.user.AdminRestController;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,22 +20,46 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class VoteServlet extends HttpServlet {
+
+    private static final Logger log = getLogger(VoteServlet.class);
+
+    private ConfigurableApplicationContext springContext;
+    private RestaurantRestController restaurantRestController;
+    private VoteRestController voteRestController;
+    private AdminRestController adminRestController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        restaurantRestController = springContext.getBean(RestaurantRestController.class);
+        voteRestController = springContext.getBean(VoteRestController.class);
+        adminRestController = springContext.getBean(AdminRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<Restaurant> restaurantList = MockUtil.RESTAURANTS;
+        User user = adminRestController.get(AuthorizedUser.id());
 
         String action = request.getParameter("action");
-
-        int voteId = MockUtil.getCounterForVote();
 
         switch (action == null ? "all" : action) {
             case "create":
             case "update":
-                Vote vote = new Vote(voteId, LocalDateTime.now());
-                MockUtil.VOTES.add(vote);
+                Restaurant restaurant = restaurantRestController.get(
+                        Integer.parseInt(request.getParameter("restId")));
+                Vote vote = new Vote(user, LocalDateTime.now(), restaurant);
+                voteRestController.create(vote);
                 /*request.setAttribute("restaurantsList", restaurantList);
                 request.getRequestDispatcher("/restaurants.jsp").forward(request, response);*/
                 response.sendRedirect("restaurants");
