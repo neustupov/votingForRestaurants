bfeac99f2b73cf362e428905083ab0c3c900d1fb
neustupov <neustupov@yandex.ru>
2018-03-04 14:19:52 +0300

    added init and @Repository annotation

--- src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryUserRepository.java
+++ src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryUserRepository.java
@@ -1,15 +1,21 @@
 package ru.neustupov.votingForRestaurants.repository.mockRepository;
 
+import org.springframework.stereotype.Repository;
+import ru.neustupov.votingForRestaurants.model.Role;
 import ru.neustupov.votingForRestaurants.model.User;
 import ru.neustupov.votingForRestaurants.repository.UserRepository;
+import sun.util.calendar.LocalGregorianCalendar;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryUserRepository implements UserRepository{
 
     public static final int USER_ID = 1;
@@ -17,6 +23,11 @@ public class InMemoryUserRepository implements UserRepository{
 
     private Map<Integer, User> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        repository.put(1, new User(USER_ID, "User", "123", Date.from(Instant.now()),  EnumSet.of(Role.ROLE_USER)));
+        repository.put(2, new User(ADMIN_ID, "Admin", "456", Date.from(Instant.now()), EnumSet.allOf(Role.class)));
+    }
 
     @Override
     public User save(User user) {
