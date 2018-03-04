28e6fe185900162243fecc9b2e3c87f0e5029f17
neustupov <neustupov@yandex.ru>
2018-03-04 20:17:54 +0300

    deleted not used imports

--- src/main/java/ru/neustupov/votingForRestaurants/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/neustupov/votingForRestaurants/repository/datajpa/CrudMealRepository.java
@@ -1,6 +1,5 @@
 package ru.neustupov.votingForRestaurants.repository.datajpa;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
