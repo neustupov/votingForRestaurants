46086ed341d7e0ce16b8f6f47dcdb81373aa9411
neustupov <neustupov@yandex.ru>
2018-03-04 14:19:08 +0300

    edited methods and added init

--- src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryRestaurantRepositoryImpl.java
+++ src/main/java/ru/neustupov/votingForRestaurants/repository/mockRepository/InMemoryRestaurantRepositoryImpl.java
@@ -1,5 +1,9 @@
 package ru.neustupov.votingForRestaurants.repository.mockRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.neustupov.votingForRestaurants.model.Restaurant;
 import ru.neustupov.votingForRestaurants.repository.RestaurantRepository;
 
@@ -10,16 +14,28 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryRestaurantRepositoryImpl implements RestaurantRepository{
+
+    private static final Logger log = LoggerFactory.getLogger(InMemoryRestaurantRepositoryImpl.class);
 
     private Map<Integer, Restaurant> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        save(new Restaurant("Topaz"));
+        save(new Restaurant("Gruzia"));
+        save(new Restaurant("Afonja"));
+        save(new Restaurant("Finland"));
+        save(new Restaurant("Ludmila"));
+    }
 
     @Override
     public Restaurant save(Restaurant restaurant) {
         if (restaurant.isNew()) {
             restaurant.setId(counter.incrementAndGet());
             repository.put(restaurant.getId(), restaurant);
+            log.info("create {} with id = {}", restaurant, restaurant.getId());
             return restaurant;
         }
         return repository.computeIfPresent(restaurant.getId(), (id, oldRestaurant) -> restaurant);
@@ -32,13 +48,13 @@ public class InMemoryRestaurantRepositoryImpl implements RestaurantRepository{
 
     @Override
     public Restaurant get(int id) {
-        return repository.get(id);
+        return repository.values().stream().filter(restaurant -> restaurant.getId() == id).findFirst().orElse(null);
     }
 
     @Override
     public List<Restaurant> getAll() {
         return repository.values().stream()
-                .sorted(Comparator.comparing(Restaurant::getName))
+                .sorted(Comparator.comparing(Restaurant::getId))
                 .collect(Collectors.toList());
     }
 
