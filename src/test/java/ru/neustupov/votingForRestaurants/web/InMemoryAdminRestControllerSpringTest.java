d7a473e029edd962d6617d0644baef50686d2e39
neustupov <neustupov@yandex.ru>
2018-03-08 22:13:18 +0300

    added spring tests for user controller

---
+++ src/test/java/ru/neustupov/votingForRestaurants/web/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,47 @@
+package ru.neustupov.votingForRestaurants.web;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.neustupov.votingForRestaurants.UserTestData;
+import ru.neustupov.votingForRestaurants.model.User;
+import ru.neustupov.votingForRestaurants.repository.mock.InMemoryUserRepository;
+import ru.neustupov.votingForRestaurants.util.exception.NotFoundException;
+import ru.neustupov.votingForRestaurants.web.user.AdminRestController;
+
+import java.util.Collection;
+
+import static ru.neustupov.votingForRestaurants.UserTestData.ADMIN;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository.init();
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(users.size(), 1);
+        Assert.assertEquals(users.iterator().next(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        controller.delete(10);
+    }
+}
