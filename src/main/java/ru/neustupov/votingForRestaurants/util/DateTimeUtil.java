7a1c0828594353622c18736bb170d1766b465215
neustupov <neustupov@yandex.ru>
2018-03-14 09:32:54 +0300

    added private constructor

--- src/main/java/ru/neustupov/votingForRestaurants/util/DateTimeUtil.java
+++ src/main/java/ru/neustupov/votingForRestaurants/util/DateTimeUtil.java
@@ -5,6 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
+    private DateTimeUtil() {
+    }
+
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static String toString(LocalDateTime ldt) {
