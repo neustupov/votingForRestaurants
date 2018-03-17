3b71d98da0a08c4eabf386e07cfabc6744311624
neustupov <neustupov@yandex.ru>
2018-03-17 20:31:12 +0300

    added User Service tests

---
+++ src/test/java/ru/neustupov/votingforrestaurants/service/UserServiceTest.java
@@ -0,0 +1,70 @@
+package ru.neustupov.votingforrestaurants.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import ru.neustupov.votingforrestaurants.model.Role;
+import ru.neustupov.votingforrestaurants.model.User;
+import ru.neustupov.votingforrestaurants.util.exception.NotFoundException;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static ru.neustupov.votingforrestaurants.UserTestData.*;
+
+public class UserServiceTest extends AbstractServiceTest{
+
+    @Autowired
+    private UserService service;
+
+    @Test
+    public void create() throws Exception {
+        User newUser = new User(null, "New", "newPass", Date.from(Instant.now()), Collections.singleton(Role.ROLE_USER));
+        User created = service.create(newUser);
+        newUser.setId(created.getId());
+        assertMatch(service.getAll(), ADMIN, newUser, USER);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void duplicateNameCreate() throws Exception {
+        service.create(new User(null, "User", "password", Date.from(Instant.now()), Collections.singleton(Role.ROLE_USER)));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        service.delete(USER_ID);
+        assertMatch(service.getAll(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void notFoundDelete() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void get() throws Exception {
+        User user = service.get(USER_ID);
+        assertMatch(user, USER);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void update() throws Exception {
+        User updated = new User(USER);
+        updated.setName("UpdatedName");
+        service.update(updated);
+        assertMatch(service.get(USER_ID), updated);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<User> all = service.getAll();
+        assertMatch(all, ADMIN, USER);
+    }
+}
