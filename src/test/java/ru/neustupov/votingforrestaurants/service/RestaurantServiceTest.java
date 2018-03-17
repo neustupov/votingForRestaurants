fa2e7d897c4f09f210f3f7da092c8a5e97dbede6
neustupov <neustupov@yandex.ru>
2018-03-17 20:29:42 +0300

    added Restaurant Service testes

---
+++ src/test/java/ru/neustupov/votingforrestaurants/service/RestaurantServiceTest.java
@@ -0,0 +1,66 @@
+package ru.neustupov.votingforrestaurants.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import ru.neustupov.votingforrestaurants.model.Restaurant;
+import ru.neustupov.votingforrestaurants.util.exception.NotFoundException;
+
+import java.util.List;
+
+import static ru.neustupov.votingforrestaurants.RestaurantTestData.*;
+
+public class RestaurantServiceTest extends AbstractServiceTest{
+
+    @Autowired
+    private RestaurantService service;
+
+    @Test
+    public void create() throws Exception {
+        Restaurant newRestaurant = new Restaurant(null, "New");
+        Restaurant created = service.create(newRestaurant);
+        newRestaurant.setId(created.getId());
+        assertMatch(service.getAll(), RUSSIA, UKRAINE, U_KOLYANA, ALMAZ, FART,  newRestaurant);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void duplicateNameCreate() throws Exception {
+        service.create(new Restaurant(null, "Russia"));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        service.delete(RUSSIA_ID);
+        assertMatch(service.getAll(), UKRAINE, U_KOLYANA, ALMAZ, FART);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void notFoundDelete() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void get() throws Exception {
+        Restaurant restaurant = service.get(UKRAINE_ID);
+        assertMatch(restaurant, UKRAINE);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void update() throws Exception {
+        Restaurant updated = new Restaurant(U_KOLYANA);
+        updated.setName("UpdatedName");
+        service.update(updated);
+        assertMatch(service.get(U_KOLYANA_ID), updated);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<Restaurant> all = service.getAll();
+        assertMatch(all, RUSSIA, UKRAINE, U_KOLYANA, ALMAZ, FART);
+    }
+}
