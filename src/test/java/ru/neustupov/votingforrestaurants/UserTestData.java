5f03c0dc8320984b43f75f20b8050949100eb877
neustupov <neustupov@yandex.ru>
2018-03-17 20:31:43 +0300

    added data for user service tests

--- src/test/java/ru/neustupov/votingforrestaurants/UserTestData.java
+++ src/test/java/ru/neustupov/votingforrestaurants/UserTestData.java
@@ -4,14 +4,30 @@ import ru.neustupov.votingforrestaurants.model.Role;
 import ru.neustupov.votingforrestaurants.model.User;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.EnumSet;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.neustupov.votingforrestaurants.model.AbstractBaseEntity.START_SEQ;
+
 public class UserTestData {
 
-    public static final int USER_ID = 1;
-    public static final int ADMIN_ID = 2;
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
 
-    public static final User USER = new User(USER_ID, "User", "123", Date.from(Instant.now()),  EnumSet.of(Role.ROLE_USER));
-    public static final User ADMIN = new User(ADMIN_ID, "Admin", "456", Date.from(Instant.now()), EnumSet.allOf(Role.class));
+    public static final User USER = new User(USER_ID, "User", "password", Date.from(Instant.now()),  EnumSet.of(Role.ROLE_USER));
+    public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin", Date.from(Instant.now()), EnumSet.of(Role.ROLE_ADMIN));
+
+    public static void assertMatch(User actual, User expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "roles", "votes");
+    }
+
+    public static void assertMatch(Iterable<User> actual, User... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("registered", "votes").isEqualTo(expected);
+    }
 }
