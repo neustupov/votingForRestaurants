c6fa45a81b27bbb3877170b9c5dfe846b03110c1
neustupov <neustupov@yandex.ru>
2018-03-17 21:08:24 +0300

    catch up date time formatter

--- src/main/java/ru/neustupov/votingforrestaurants/util/DateTimeUtil.java
+++ src/main/java/ru/neustupov/votingforrestaurants/util/DateTimeUtil.java
@@ -5,10 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     private DateTimeUtil() {
     }
-
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
