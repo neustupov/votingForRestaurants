35943334bd0a1aead721caa9dd0b37f6df798c01
neustupov <neustupov@yandex.ru>
2018-03-17 20:29:06 +0300

    edited users, restaurants and meals data

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -12,8 +12,7 @@ INSERT INTO USERS (name, password) VALUES
 
 INSERT INTO USER_ROLES (role, user_id) VALUES
   ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001),
-  ('ROLE_USER', 100001);
+  ('ROLE_ADMIN', 100001);
 
 INSERT INTO RESTAURANTS (NAME) VALUES
   ('Russia'),
@@ -27,14 +26,16 @@ INSERT INTO MENUS (ID_REST, ADD_DATE) VALUES
   (100003, '2015-05-02 09:00:00'),
   (100004, '2015-05-03 10:00:00'),
   (100005, '2015-05-01 08:00:00'),
-  (100006, '2015-05-02 07:00:00');
+  (100006, '2015-05-02 07:00:00'),
+  (100002, '2015-05-02 08:00:00');
 
 INSERT INTO MEALS (ID_MENU, NAME, PRICE) VALUES
-  (100007, 'Завтрак', 500),
-  (100008, 'Завтрак', 500),
-  (100009, 'Завтрак', 500),
-  (100010, 'Завтрак', 500),
-  (100011,  'Завтрак', 500);
+  (100007, 'Apple', 5),
+  (100008, 'Juice', 10),
+  (100009, 'Potato', 20),
+  (100010, 'Bananas', 30),
+  (100011, 'Bread', 40),
+  (100007, 'Bottle of water', 50);
 
 INSERT INTO VOTES (USER_ID, DATE_TIME, RESTAURANT) VALUES
   (100000, '2015-05-01 10:00:00', 100002),
