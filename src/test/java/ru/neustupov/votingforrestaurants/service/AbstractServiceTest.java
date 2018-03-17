3a6629704b3854e2bdbd47bdb98bfc0c01aca56a
neustupov <neustupov@yandex.ru>
2018-03-17 20:07:06 +0300

    added abstract class for all service testes

---
+++ src/test/java/ru/neustupov/votingforrestaurants/service/AbstractServiceTest.java
@@ -0,0 +1,13 @@
+package ru.neustupov.votingforrestaurants.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+abstract public class AbstractServiceTest {
+}
