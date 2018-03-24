f984603e3a269ab4a7ba3508cdf4ea58b0c062d4
neustupov <neustupov@yandex.ru>
2018-03-24 21:38:26 +0300

    added validateRootCause method for compare the throws exceptions

--- src/test/java/ru/neustupov/votingforrestaurants/service/AbstractServiceTest.java
+++ src/test/java/ru/neustupov/votingforrestaurants/service/AbstractServiceTest.java
@@ -1,13 +1,28 @@
 package ru.neustupov.votingforrestaurants.service;
 
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static ru.neustupov.votingforrestaurants.util.ValidationUtil.getRootCause;
+
 @ContextConfiguration("classpath:spring/spring-app.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 abstract public class AbstractServiceTest {
+
+    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
+    public <T extends Throwable> void validateRootCause(Runnable runnable, Class<T> exceptionClass) {
+        try {
+            runnable.run();
+            Assert.fail("Expected " + exceptionClass.getName());
+        } catch (Exception e) {
+            Assert.assertThat(getRootCause(e), instanceOf(exceptionClass));
+        }
+    }
+
 }
