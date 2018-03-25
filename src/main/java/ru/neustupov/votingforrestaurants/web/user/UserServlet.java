6008dde0c562111d873e3aebc0dfeb364157a3fc
neustupov <neustupov@yandex.ru>
2018-03-25 18:24:51 +0300

    added init context and edited doGet method

--- src/main/java/ru/neustupov/votingforrestaurants/web/user/UserServlet.java
+++ src/main/java/ru/neustupov/votingforrestaurants/web/user/UserServlet.java
@@ -1,8 +1,11 @@
 package ru.neustupov.votingforrestaurants.web.user;
 
 import org.slf4j.Logger;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.neustupov.votingforrestaurants.AuthorizedUser;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +17,16 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class UserServlet extends HttpServlet{
     private static final Logger log = getLogger(UserServlet.class);
 
+
+    private AdminRestController adminController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        adminController = springContext.getBean(AdminRestController.class);
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int userId = Integer.parseInt(request.getParameter("userId"));
@@ -23,7 +36,8 @@ public class UserServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("forward to users");
+        log.debug("getAll");
+        request.setAttribute("users", adminController.getAll());
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
