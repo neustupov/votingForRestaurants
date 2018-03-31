592acd7655c6d32de642e4a88cf910dddf757b0a
neustupov <neustupov@yandex.ru>
2018-03-31 20:35:51 +0300

    added controllers instead servlets

--- src/main/java/ru/neustupov/votingforrestaurants/web/restaurant/JspRestaurantController.java
+++ src/main/java/ru/neustupov/votingforrestaurants/web/restaurant/JspRestaurantController.java
@@ -1,4 +1,57 @@
 package ru.neustupov.votingforrestaurants.web.restaurant;
 
-public class JspRestaurantController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.neustupov.votingforrestaurants.model.Restaurant;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+@Controller
+@RequestMapping(value = "/restaurants")
+public class JspRestaurantController extends AbstractRestaurantController{
+
+    @GetMapping
+    public String restaurants(Model model) {
+        model.addAttribute("restaurantsList", super.getAll());
+        return "restaurants";
+    }
+
+    @GetMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        super.delete(getId(request));
+        return "redirect:/restaurants";
+    }
+
+    @GetMapping("/update")
+    public String update(HttpServletRequest request, Model model) {
+        model.addAttribute("restaurant", super.get(getId(request)));
+        return "restaurantForm";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("restaurant", new Restaurant());
+        return "restaurantForm";
+    }
+
+    @PostMapping
+    public String updateOrCreate(HttpServletRequest request) {
+        Restaurant restaurant = new Restaurant(request.getParameter("name"));
+
+        if (request.getParameter("id").isEmpty()) {
+            super.create(restaurant);
+        } else {
+            super.update(restaurant, getId(request));
+        }
+        return "redirect:/restaurants";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
+    }
 }
