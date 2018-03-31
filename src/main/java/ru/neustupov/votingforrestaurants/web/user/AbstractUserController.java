bde704f5c4f1e601c44be8198cad22892b5a29e7
neustupov <neustupov@yandex.ru>
2018-03-31 20:16:27 +0300

    added abstract controller for users

--- src/main/java/ru/neustupov/votingforrestaurants/web/user/AbstractUserController.java
+++ src/main/java/ru/neustupov/votingforrestaurants/web/user/AbstractUserController.java
@@ -1,4 +1,47 @@
 package ru.neustupov.votingforrestaurants.web.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.neustupov.votingforrestaurants.model.User;
+import ru.neustupov.votingforrestaurants.service.UserService;
+
+import java.util.List;
+
+import static ru.neustupov.votingforrestaurants.util.ValidationUtil.assureIdConsistent;
+import static ru.neustupov.votingforrestaurants.util.ValidationUtil.checkNew;
+
 public abstract class AbstractUserController {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserService service;
+
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        return service.create(user);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(User user, int id) {
+        log.info("update {} with id={}", user, id);
+        assureIdConsistent(user, id);
+        service.update(user);
+    }
 }
