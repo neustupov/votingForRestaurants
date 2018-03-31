9cef585560fbea4569e6a4ac321f7b5c6712dae8
neustupov <neustupov@yandex.ru>
2018-03-31 21:20:36 +0300

    added variable in save method

--- src/main/java/ru/neustupov/votingforrestaurants/repository/VoteRepository.java
+++ src/main/java/ru/neustupov/votingforrestaurants/repository/VoteRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface VoteRepository {
 
-    Vote save(Vote vote, int userId);
+    Vote save(Vote vote, int userId, int restId);
 
     // false if not found
     boolean delete(int id, int userId);
