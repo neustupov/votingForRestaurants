c1de69263014de5fa8d0abd786c8f23a0ca07d7e
neustupov <neustupov@yandex.ru>
2018-03-31 20:16:02 +0300

    added abstract controller for restaurant

--- src/main/java/ru/neustupov/votingforrestaurants/web/restaurant/AbstractRestaurantController.java
+++ src/main/java/ru/neustupov/votingforrestaurants/web/restaurant/AbstractRestaurantController.java
@@ -1,4 +1,53 @@
 package ru.neustupov.votingforrestaurants.web.restaurant;
 
-public class AbstractRestaurantController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.neustupov.votingforrestaurants.model.Restaurant;
+import ru.neustupov.votingforrestaurants.service.RestaurantService;
+import ru.neustupov.votingforrestaurants.service.VoteService;
+import ru.neustupov.votingforrestaurants.to.RestaurantWithVotes;
+import ru.neustupov.votingforrestaurants.util.RestaurantsUtil;
+
+import java.util.List;
+
+import static ru.neustupov.votingforrestaurants.util.ValidationUtil.assureIdConsistent;
+import static ru.neustupov.votingforrestaurants.util.ValidationUtil.checkNew;
+
+public abstract class AbstractRestaurantController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private VoteService voteService;
+
+    public Restaurant get(int id) {
+        log.info("get restaurant {}", id);
+        return restaurantService.get(id);
+    }
+
+    public void delete(int id) {
+        log.info("delete restaurant {}", id);
+        restaurantService.delete(id);
+    }
+
+    public List<RestaurantWithVotes> getAll() {
+        log.info("getAll restaurants {}");
+        return RestaurantsUtil.getWithVotes(restaurantService.getAll(),voteService.getAll());
+    }
+
+    public Restaurant create(Restaurant restaurant) {
+        checkNew(restaurant);
+        log.info("create restaurant {}", restaurant.getName());
+        return restaurantService.create(restaurant);
+    }
+
+    public void update(Restaurant restaurant, int id) {
+        assureIdConsistent(restaurant, id);
+        log.info("update restaurant {}",restaurant.getId());
+        restaurantService.update(restaurant);
+    }
 }
