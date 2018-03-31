4015745601b6c7fe129a7199c156568946cee2dd
neustupov <neustupov@yandex.ru>
2018-03-31 21:15:21 +0300

    edited localDateTime to date

--- src/test/java/ru/neustupov/votingforrestaurants/MenuTestData.java
+++ src/test/java/ru/neustupov/votingforrestaurants/MenuTestData.java
@@ -2,11 +2,10 @@ package ru.neustupov.votingforrestaurants;
 
 import ru.neustupov.votingforrestaurants.model.Menu;
 
-import java.time.Month;
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.time.LocalDateTime.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static ru.neustupov.votingforrestaurants.model.AbstractBaseEntity.START_SEQ;
 
@@ -19,17 +18,17 @@ public class MenuTestData {
     public static final int FART_MENU_ID = START_SEQ + 11;
     public static final int RUSSIA_MENU_ID2 = START_SEQ + 12;
 
-    public static final Menu RUSSIA_MENU1 = new Menu(RUSSIA_MENU_ID1, of(2015, Month.MAY, 1, 8, 0));
-    public static final Menu UKRAINE_MENU = new Menu(UKRAINE_MENU_ID, of(2015, Month.MAY, 2, 9, 0));
-    public static final Menu U_KOLYANA_MENU = new Menu(U_KOLYANA_MENU_ID, of(2015, Month.MAY, 3, 10, 0));
-    public static final Menu ALMAZ_MENU = new Menu(ALMAZ_MENU_ID, of(2015, Month.MAY, 1, 8, 0));
-    public static final Menu FART_MENU = new Menu(FART_MENU_ID, of(2015, Month.MAY, 2, 7, 0));
-    public static final Menu RUSSIA_MENU2 = new Menu(RUSSIA_MENU_ID2, of(2015, Month.MAY, 2, 8, 0));
+    public static final Menu RUSSIA_MENU1 = new Menu(RUSSIA_MENU_ID1, Date.valueOf("2015-05-01"));
+    public static final Menu UKRAINE_MENU = new Menu(UKRAINE_MENU_ID, Date.valueOf("2015-05-02"));
+    public static final Menu U_KOLYANA_MENU = new Menu(U_KOLYANA_MENU_ID, Date.valueOf("2015-05-03"));
+    public static final Menu ALMAZ_MENU = new Menu(ALMAZ_MENU_ID, Date.valueOf("2015-05-01"));
+    public static final Menu FART_MENU = new Menu(FART_MENU_ID, Date.valueOf("2015-05-02"));
+    public static final Menu RUSSIA_MENU2 = new Menu(RUSSIA_MENU_ID2, Date.valueOf("2015-05-02"));
 
     public static final List<Menu> MENUS = Arrays.asList(RUSSIA_MENU1, UKRAINE_MENU, U_KOLYANA_MENU, ALMAZ_MENU, FART_MENU, RUSSIA_MENU2);
 
     public static Menu getCreated() {
-        return new Menu(null, of(2015, Month.JUNE, 1, 18, 0));
+        return new Menu(null, Date.valueOf("2015-06-01"));
     }
 
     public static void assertMatch(Menu actual, Menu expected) {
