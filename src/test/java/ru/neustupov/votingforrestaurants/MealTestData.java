c9df6189a2f476ed85c0f189c8f0b1739422a236
neustupov <neustupov@yandex.ru>
2018-04-01 19:36:10 +0300

    added mealInMenu

--- src/test/java/ru/neustupov/votingforrestaurants/MealTestData.java
+++ src/test/java/ru/neustupov/votingforrestaurants/MealTestData.java
@@ -16,6 +16,7 @@ public class MealTestData {
     public static final int BANANAS_ID = START_SEQ + 16;
     public static final int BREAD_ID = START_SEQ + 17;
     public static final int BOTTLE_OF_WATER_ID = START_SEQ + 18;
+    public static final int MEAL_IN_MENU_ID = START_SEQ + 20;
 
     public static final Meal APPLE = new Meal(APPLE_ID, "Apple", 5);
     public static final Meal JUICE = new Meal(JUICE_ID, "Juice", 10);
@@ -23,6 +24,8 @@ public class MealTestData {
     public static final Meal BANANAS = new Meal(BANANAS_ID, "Bananas", 30);
     public static final Meal BREAD = new Meal(BREAD_ID, "Bread", 40);
     public static final Meal BOTTLE_OF_WATER = new Meal(BOTTLE_OF_WATER_ID, "Bottle of water", 50);
+
+    public static final Meal MEAL_IN_MENU = new Meal(MEAL_IN_MENU_ID, "Mango", 100);
 
     public static final List<Meal> MEALS = Arrays.asList(APPLE, JUICE, POTATO, BANANAS, BREAD, BOTTLE_OF_WATER);
 
