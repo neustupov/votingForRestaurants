7cedf8b0b7f0b3c98dde5ca0e2eb58ff8dcf2c06
neustupov <neustupov@yandex.ru>
2018-04-03 09:28:54 +0300

    added class for testing root controller

---
+++ src/test/java/ru/neustupov/votingforrestaurants/web/RootControllerTest.java
@@ -0,0 +1,32 @@
+package ru.neustupov.votingforrestaurants.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static ru.neustupov.votingforrestaurants.UserTestData.USER;
+import static ru.neustupov.votingforrestaurants.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest{
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
