1528cdfb7c97f1f6242df23016bda56d6b9d76fb
neustupov <neustupov@yandex.ru>
2018-04-06 17:03:35 +0300

    add couples of tests - getByUserIdAndDate and getByUserIdAndDateNotFound

--- src/test/java/ru/neustupov/votingforrestaurants/service/VoteServiceTest.java
+++ src/test/java/ru/neustupov/votingforrestaurants/service/VoteServiceTest.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.neustupov.votingforrestaurants.model.Vote;
 import ru.neustupov.votingforrestaurants.util.exception.NotFoundException;
 
+import java.sql.Date;
+import java.time.Instant;
+
 import static ru.neustupov.votingforrestaurants.RestaurantTestData.RUSSIA_ID;
 import static ru.neustupov.votingforrestaurants.UserTestData.ADMIN_ID;
 import static ru.neustupov.votingforrestaurants.UserTestData.USER_ID;
 import static ru.neustupov.votingforrestaurants.VoteTestData.*;
 
-public class VoteServiceTest extends AbstractServiceTest{
+public class VoteServiceTest extends AbstractServiceTest {
 
     @Autowired
     private VoteService service;
@@ -46,6 +49,17 @@ public class VoteServiceTest extends AbstractServiceTest{
 
     @Test
     public void getAll() throws Exception {
-        assertMatch(service.getAll(),VOTE1, VOTE2, VOTE3, VOTE4, VOTE5, VOTE6);
+        assertMatch(service.getAll(), VOTE1, VOTE2, VOTE3, VOTE4, VOTE5, VOTE6);
+    }
+
+    @Test
+    public void getByUserIdAndDate() throws Exception {
+        service.create(new Vote(Date.from(Instant.now())), ADMIN_ID, RUSSIA_ID);
+        assertMatch(service.getByUserIdAndDate(ADMIN_ID), VOTE_FOR_GET_BY_USER_ID_AND_DATE);
+    }
+
+    @Test
+    public void getByUserIdAndDateNotFound() throws Exception {
+        assertMatch(service.getByUserIdAndDate(ADMIN_ID), null);
     }
 }
