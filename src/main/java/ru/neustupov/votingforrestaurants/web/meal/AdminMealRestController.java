129856a1cd4f3fec6efb9216104eeef2b9a48d04
neustupov <neustupov@yandex.ru>
2018-04-06 14:35:53 +0300

    added admin rest controller for meal

---
+++ src/main/java/ru/neustupov/votingforrestaurants/web/meal/AdminMealRestController.java
@@ -0,0 +1,52 @@
+package ru.neustupov.votingforrestaurants.web.meal;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.neustupov.votingforrestaurants.model.Meal;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminMealRestController.REST_URL)
+public class AdminMealRestController extends AbstractMealController{
+
+    static final String REST_URL = "/rest/admin/meals";
+
+    @Override
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Meal get(@PathVariable("id") int id, @RequestParam("menuId") int menuId) {
+        return super.get(id, menuId);
+    }
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Meal> getAll(@RequestParam("menuId") int menuId) {
+        return super.getAll(menuId);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal, @RequestParam("menuId") int menuId) {
+        Meal created = super.create(meal, menuId);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @Override
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Meal meal, @RequestParam("mealId") int mealId, @RequestParam("menuId") int menuId) {
+        super.update(meal, mealId, menuId);
+    }
+
+    @Override
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") int id, @RequestParam("menuId") int menuId) {
+        super.delete(id, menuId);
+    }
+}
