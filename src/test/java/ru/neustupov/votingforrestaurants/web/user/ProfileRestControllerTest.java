7268f6c8518b2beb0740bd9fe287753863419da3
neustupov <neustupov@yandex.ru>
2018-04-07 20:32:48 +0300

    added tests methods for testing profile rest controller

---
+++ src/test/java/ru/neustupov/votingforrestaurants/web/user/ProfileRestControllerTest.java
@@ -0,0 +1,53 @@
+package ru.neustupov.votingforrestaurants.web.user;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import ru.neustupov.votingforrestaurants.TestUtil;
+import ru.neustupov.votingforrestaurants.model.Role;
+import ru.neustupov.votingforrestaurants.model.User;
+import ru.neustupov.votingforrestaurants.web.AbstractControllerTest;
+import ru.neustupov.votingforrestaurants.web.json.JsonUtil;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.util.EnumSet;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.neustupov.votingforrestaurants.UserTestData.*;
+import static ru.neustupov.votingforrestaurants.web.user.ProfileRestController.REST_URL;
+
+public class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testGet() throws Exception {
+        TestUtil.print(
+                mockMvc.perform(get(REST_URL))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(contentJson(USER))
+        );
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL))
+                .andExpect(status().isOk());
+        assertMatch(userService.getAll(), ADMIN);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User updated = new User(USER_ID, "newName", "newPassword", Date.from(Instant.now()), EnumSet.of(Role.ROLE_USER));
+        mockMvc.perform(put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        assertMatch(new User(userService.get(USER_ID)), updated);
+    }
+}
