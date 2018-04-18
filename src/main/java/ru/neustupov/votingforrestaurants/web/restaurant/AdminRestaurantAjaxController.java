52b62e8d07b4d1a720ea64ece1b6bd573fd6f6ea
neustupov <neustupov@yandex.ru>
2018-04-18 12:04:38 +0300

    added admin ajax controller for restaurant

---
+++ src/main/java/ru/neustupov/votingforrestaurants/web/restaurant/AdminRestaurantAjaxController.java
@@ -0,0 +1,40 @@
+package ru.neustupov.votingforrestaurants.web.restaurant;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.neustupov.votingforrestaurants.model.Restaurant;
+import ru.neustupov.votingforrestaurants.to.RestaurantWithVotes;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/ajax/admin/restaurants")
+public class AdminRestaurantAjaxController extends AbstractRestaurantController{
+
+    @Override
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Restaurant get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RestaurantWithVotes> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                               @RequestParam("name") String name) {
+        Restaurant restaurant = new Restaurant(id, name);
+        if (restaurant.isNew()) {
+            super.create(restaurant);
+        }
+    }
+}
