c0edafac93891e812aaeaf88ed6a18e11e0a3d1a
neustupov <neustupov@yandex.ru>
2018-04-24 17:12:28 +0300

    added extends from BaseTo

--- src/main/java/ru/neustupov/votingforrestaurants/to/UserTo.java
+++ src/main/java/ru/neustupov/votingforrestaurants/to/UserTo.java
@@ -1,6 +1,6 @@
 package ru.neustupov.votingforrestaurants.to;
 
-public class UserTo {
+public class UserTo extends BaseTo{
 
     private Integer id;
 
@@ -14,18 +14,10 @@ public class UserTo {
     }
 
     public UserTo(Integer id, String name, String email, String password) {
-        this.id = id;
+        super(id);
         this.name = name;
         this.email = email;
         this.password = password;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getPassword() {
@@ -50,10 +42,6 @@ public class UserTo {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public boolean isNew() {
-        return id == null;
     }
 
     @Override
