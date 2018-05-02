27f67293be841695dea2a18ccb2c7d4911e3f607
neustupov <neustupov@yandex.ru>
2018-05-02 20:08:09 +0300

    added implementing GrantedAuthority interface

--- src/main/java/ru/neustupov/votingforrestaurants/model/Role.java
+++ src/main/java/ru/neustupov/votingforrestaurants/model/Role.java
@@ -1,6 +1,13 @@
 package ru.neustupov.votingforrestaurants.model;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
