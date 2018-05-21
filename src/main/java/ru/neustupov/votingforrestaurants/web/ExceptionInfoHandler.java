a675451e3329dd0a42bebac995825293e5be8474
neustupov <neustupov@yandex.ru>
2018-05-21 18:36:16 +0300

    added error handling 404

--- src/main/java/ru/neustupov/votingforrestaurants/web/ExceptionInfoHandler.java
+++ src/main/java/ru/neustupov/votingforrestaurants/web/ExceptionInfoHandler.java
@@ -81,6 +81,6 @@ public class ExceptionInfoHandler {
         }
         return new ErrorInfo(req.getRequestURL(), errorType,
                 messageUtil.getMessage(errorType.getErrorCode()),
-                details.length != 0 ? details : new String[]{rootCause.toString()});
+                details.length != 0 ? details : new String[]{ValidationUtil.getMessage(rootCause)});
     }
 }
